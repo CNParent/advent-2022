01ef7b11996d185360a2d5fc16cb6e309fd3f449
Chris Parent <chrisnparent@gmail.com>
2022-12-04 14:32:52 -0500

    day 4

--- day4/2.js
+++ day4/2.js
@@ -1,4 +1,20 @@
 scripts.day4_2 = async () => {
     let data = await getFile('day4/input.txt');
-    terminal.textContent = `Answer here!`;
+    let pairs = data.split('\r\n').map(x => x.split(',').map(y => y.split('-'))).map(x => {
+        return {
+            a: { min: Number(x[0][0]), max: Number(x[0][1]) },
+            b: { min: Number(x[1][0]), max: Number(x[1][1]) }
+        }
+    });
+
+    let overlapping = pairs.filter(pair => {
+        let a = pair.a;
+        let b = pair.b;
+
+        if (a.min > b.max || a.max < b.min) return false;
+
+        return true;
+    });
+
+    terminal.textContent = `There are ${overlapping.length} pairs with overlapping ids`;
 }
