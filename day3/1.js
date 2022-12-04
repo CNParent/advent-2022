a39f95b0252e6cd6eb6e430f1ec844e3d88bb6ab
Chris Parent <chrisnparent@gmail.com>
2022-12-04 14:06:58 -0500

    day 3

--- day3/1.js
+++ day3/1.js
@@ -1,4 +1,18 @@
 scripts.day3_1 = async () => {
+    const items = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let data = await getFile('day3/input.txt');
-    terminal.textContent = `Answer here!`;
+    let sacks = data.split('\r\n').map(x => {
+        return {
+            a: x.slice(0, x.length / 2).split(''),
+            b: x.slice(x.length / 2).split('')
+        }
+    });
+
+    let total = 0;
+    sacks.forEach(sack => {
+        let dup = sack.a.find(x => sack.b.includes(x));
+        total += items.indexOf(dup) + 1;
+    });
+
+    terminal.textContent = `Sum of all priorities is ${total}`;
 }
--- day3/2.js
+++ day3/2.js
@@ -1,4 +1,12 @@
 scripts.day3_2 = async () => {
+    const items = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let data = await getFile('day3/input.txt');
-    terminal.textContent = `Answer here!`;
+    let sacks = data.split('\r\n').map(x => x.split(''));
+    let total = 0;
+    for(i = 0; i < sacks.length; i += 3) {
+        let badge = sacks[i].find(x => sacks[i + 1].includes(x) && sacks[i+2].includes(x));
+        total += items.indexOf(badge) + 1;
+    }
+
+    terminal.textContent = `Sum of all priorities is ${total}`;
 }
